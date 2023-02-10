4984c7827b16fada46ecb79ed5eae0cfc932bd3e
Yosuke Toyota <1994851+yo-to@users.noreply.github.com>
2023-02-10 12:22:06 +0900

    Create p5-sketch-sample.js

---
+++ p5-sketch-sample.js
@@ -0,0 +1,14 @@
+// 取得元： https://github.com/githubnext/blocks/blob/main/examples/p5-sketch.js
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  fill(0);
+  textSize(width / 60);
+  textAlign(CENTER);
+  text("Move your mouse around the screen", width / 2, height / 3);
+}
+
+function draw() {
+  fill(mouseX, mouseY, mouseX + mouseY, 20);
+  ellipse(mouseX, mouseY, abs(pmouseX - mouseX) * 2, abs(pmouseX - mouseX) * 2);
+}
